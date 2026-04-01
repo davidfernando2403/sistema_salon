fed9525b80eec7b24c5409c1e9775d9506b3b2f2
David Ñique <davidfernando2403@gmail.com>
2026-04-01 00:08:13 -0500

    fix index

--- app.py
+++ app.py
@@ -77,7 +77,9 @@ from routes.graficos import graficos_bp
 from routes.boletas import boletas_bp
 from routes.comisiones import comisiones_bp
 from routes.auth import auth_bp
+from routes.main_routes import main_bp
 
+app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(comisiones_bp)
 app.register_blueprint(boletas_bp)
